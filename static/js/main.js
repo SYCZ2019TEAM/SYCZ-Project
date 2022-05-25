dce5640057e06268e679cb0efd56edb2b1aaf205
SYCZ2019 <105980017+SYCZ2019@users.noreply.github.com>
2022-05-26 01:25:35 +0800

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -3,6 +3,6 @@ const init = new page.Init;
 const time = new page.Time;
 init.loadImage();
 init.getBackground('./static/img/1920.jpg','image/JPEG');
-const setInterval(function(){
+const clock = setInterval(function(){
     document.getElementsByClassName('time')[0].innerHTML = time.getTime();
 }, 50);
