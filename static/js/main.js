43035b0c5449d2d64fecf7db2c5e0d0f55454ba0
SYCZ2019 <105980017+SYCZ2019@users.noreply.github.com>
2022-05-26 01:24:24 +0800

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,8 @@
 import { page } from "./functions.js";
 const init = new page.Init;
+const time = new page.Time;
 init.loadImage();
 init.getBackground('./static/img/1920.jpg','image/JPEG');
+const setInterval(function(){
+    document.getElementsByClassName('time')[0].innerHTML = time.getTime();
+}, 50);
