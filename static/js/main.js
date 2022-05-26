228c584b0040afbb8640d649640fe1966d729d3c
SYCZ2019 <105980017+SYCZ2019@users.noreply.github.com>
2022-05-26 22:52:42 +0800

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -6,8 +6,8 @@ init.getBackground('./static/img/1920.jpg','image/JPEG');
 const clock = setInterval(function(){
     document.getElementsByClassName('time')[0].innerHTML = time.getTime();
 }, 50);
-const s-button = getElementById('search');
-const s-box = getElementById('box');
+const s-button = document.getElementById('search');
+const s-box = document.getElementById('box');
 s-button.addEventListener('click',function(){
     window.open('https://baidu.com/s?wd=' + s-box.value,'_blank');
 });
