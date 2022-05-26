2d4f171df9689d7ca31d7f49115a890b47b00581
SYCZ2019 <105980017+SYCZ2019@users.noreply.github.com>
2022-05-26 23:25:26 +0800

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -13,5 +13,6 @@ s_button.addEventListener('click',function(){
     window.open('https://baidu.com/s?wd=' + s_box.value,'_blank');
 });
 s_box.addEventListener('keypress',function(e){
-    let keyCode = null; if(e.which) keyCode = e.which; else if(e.keyCode) keyCode = e.keyCode; if(keyCode == 13) { window.open('https://baidu.com/s?wd=' + s_box.value,'_blank'); };
+    let keyCode = null;
+    if(e.which) keyCode = e.which; else if(e.keyCode) keyCode = e.keyCode; if(keyCode == 13) { window.open('https://baidu.com/s?wd=' + s_box.value,'_blank'); };
 });
