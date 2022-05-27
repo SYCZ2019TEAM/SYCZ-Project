a59d6b3fe7c9aafa228b295436feacef2d681ce9
SYCZ2019 <105980017+SYCZ2019@users.noreply.github.com>
2022-05-28 00:15:39 +0800

    Update settings.js

--- static/js/settings.js
+++ static/js/settings.js
@@ -3,19 +3,35 @@ const init = new page.Init;
 const cookie = new page.Cookie;
 const setting = {
     "s_engine":{
-        "se_baidu":"https://baidu.com/s?wd=",
-        "se_bing":"https://bing.com/search?q=",
-        "se_google":"https://google.com/search?q="
+        {
+            "name":"se_bing",
+            "url":"https://bing.com/search?q="
+        },
+        {
+            "name":"se_google",
+            "url":"https://google.com/search?q="
+        },
+        {
+            "name":"se_baidu",
+            "url":"https://baidu.com/s?wd="
+        }
     }
 };
 const set_se = document.getElementsByName("set-se");
+let searchUrl = cookie.get('searchUrl');
+if(searchUrl == null){
+    searchUrl = "https://bing.com/search?q=";
+}
+for(let i = 0; i < set_se.length; i++){
+    if(setting['s_engine'][i]['url'] == searchUrl){
+        set_se[i].children[0].checked = true;
+    }
+}
 for(let i = 0; i < set_se.length; i++){
     set_se[i].children[0].addEventListener('change', function(){
-        for(let i = 0; i < set_se.length; i++){
-            if(set_se[i].children[0].checked){
-                cookie.set('searchUrl',setting['s_engine'][set_se[i].children[0].id],365);
-                console.log(setting['s_engine'][set_se[i].children[0].id]);
-            }
+        if(set_se[i].children[0].checked){
+            cookie.set('searchUrl',setting['s_engine'][i]['url'],365);
+            console.log(setting['s_engine'][i]['url']);
         }
     });
 }
