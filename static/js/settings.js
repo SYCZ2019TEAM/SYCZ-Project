1f07571d8bb39c1905f1c3117c515592f2e0d9b1
SYCZ2019 <105980017+SYCZ2019@users.noreply.github.com>
2022-05-27 23:25:53 +0800

    Update settings.js

--- static/js/settings.js
+++ static/js/settings.js
@@ -1,4 +1,13 @@
 import { page } from "./functions.js";
 const init = page.Init;
 const cookie = page.Cookie;
+const setting = {
+    "s_engine":{
+        "baidu":"https://baidu.com/s?wd=",
+        "bing":"https://bing.com/search?q=",
+        "google":"https://google.com/search?q="
+    }
+};
+const set_se = document.getSelectorAll("input[name='s_engine']");
+console.log(set_se);
 init.loadImage();
