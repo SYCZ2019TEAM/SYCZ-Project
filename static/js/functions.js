c556195f3c75ebc81e487d3703c7fbef0112f769
SYCZ2019 <105980017+SYCZ2019@users.noreply.github.com>
2022-06-05 01:19:43 +0800

    Update functions.js

--- static/js/functions.js
+++ static/js/functions.js
@@ -138,9 +138,8 @@ export const plugin = {
                 }
             }
             else if(_type == "usd"){
-                const _result = _net.getJson("https://www.mxnzp.com/api/exchange_rate/aim?from=USD&to=CNY&app_id=onxudwg6nriqlluz&app_secret=bjhSOVpJbTE5ZmUvSDYvak93cGt3QT09")["result"];
-                const _rate = _result['rate'];
-                return `<div class="p-box p-usd"><p>USD</p><h1>1.0000</h1><p>CNY</p><h1>${_rate}</h1></div>`;
+                const _result = _net.getJson("https://www.mxnzp.com/api/exchange_rate/aim?from=USD&to=CNY&app_id=onxudwg6nriqlluz&app_secret=bjhSOVpJbTE5ZmUvSDYvak93cGt3QT09")["data"]["price"];
+                return `<div class="p-box p-usd"><p>USD</p><h1>1.0000</h1><p>CNY</p><h1>${_result}</h1></div>`;
             }
             else{
                 return "Undefined Type!";
