27e066c5e02d786faca9604605f82be3b2532fa9
SYCZ2019 <105980017+SYCZ2019@users.noreply.github.com>
2022-06-05 01:07:34 +0800

    Update functions.js

--- static/js/functions.js
+++ static/js/functions.js
@@ -138,7 +138,7 @@ export const plugin = {
                 }
             }
             else if(_type == "usd"){
-                const _result = _net.getJson("https://sapi.k780.com/?app=finance.rate&scur=USD&tcur=CNY&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4")["result"];
+                const _result = _net.getJson("https://sapi.k780.com/?app=finance.rate&scur=USD&tcur=CNY&appkey=66100&sign=9ea2a84c2c6dedc689b1b4a904aadbc5")["result"];
                 const _rate = _result['rate'];
                 const _update = _result['update'];
                 return `<div class="p-box p-usd"><p>USD</p><h1>1.0000</h1><p>CNY</p><h1>${_rate}</h1><p style="color:rgb(165,165,165)"></p>更新时间: ${_update}</div>`;
