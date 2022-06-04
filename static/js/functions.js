ce0d41881f4c8349a7b0d74d5dc8608142e74276
SYCZ2019 <105980017+SYCZ2019@users.noreply.github.com>
2022-06-05 02:27:31 +0800

    Update functions.js

--- static/js/functions.js
+++ static/js/functions.js
@@ -22,7 +22,7 @@ export const page = {
             let query = window.location.search.substring(1);
             let vars = query.split("&");
             for (let i=0;i<vars.length;i++) {
-               let pair = lets[i].split("=");
+               let pair = vars[i].split("=");
                if(pair[0] == variable){return pair[1];}
             }
             return(false);
